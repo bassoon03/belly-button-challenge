8bf926317f1a6cdd44c396fdf0ced4119e6b70ed
Brent Lang <bassoon03@gmail.com>
2024-03-26 14:47:11 -0700

    Read in data

--- static/js/app.js
+++ static/js/app.js
@@ -1,120 +1,54 @@
+// Url for json file
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-//const sampleJson = d3.json(url);
-//console.log("C:\Users\Owner\belly-button-challenge\samples.json", sampleJson);
 
 
-function init(){
-    d3.json(url).then(function(data)};
-init()
-//sampleJson.then(function(data) {
-//    console.log(data);
-//});
+// Read in file from url
+d3.json(url).then(function(data){
+    values = data.samples;
+    meta_data = data.metadata;
+    Names = data.names;
+    value10 = values.sample_values.slice(0,10);
+    ids10 = values.otu_ids.slice(0,10);
+    labels10 = values.otu_labels.slice(0,10);
+});
 
-
-
-
-
-
-
-// For loop and lists will be created here.
-
-let values = [];
-let otuIds = [];
-let otuLabels = [];
-
-// Loop to create lists
-// Create similar loop for bubble chart data
-for (let i = 0; i < sampleJson.length; i++) {
-    row = samples[1];
-    const sortedSamples = points.slice().sort(function(a,b) {
-        return b-a;
-    });
-    const sortedIds = [];
-    const sortedLabels = [];
-    sortedSamples.forEach(function(val) {
-        const position = row.values.indexOf(val);
-        sortedIds.push(row.otu_ids[position]);
-        sortedLabels.push(row.otu_labels[position]);
-    });
-    values.push(sortedSamples);
-    otuIds.push(sortedIds);
-    otuLabels.push(sortedLabels);
-};
+// When new dropdown menu option selected, optionChanged called
+d3.selectAll("#selDataset").on("onchange", optionChanged);
 
 
 
-
-
-
-
-
-bar_data = [];
-
-for (let i = 0; i < values.length; i++) {
+function init() {
     const trace = {
-        x : values[i],
-        y : otuIds,
-        text : otuLabels,
+        x : value10,
+        y : ids10,
+        text : lables10,
         type : 'bar',
         orientation : 'h'
     };
-    data.push(trace);
-};
 
-
-
-
-bubble_data = [];
-
-for (let i = 0; i < values.length; i++) {
-    const trace = {
+    const bubble_trace = {
         x : values[i],
         y : otuIds,
         mode : 'markers',
         markers : {
             size : 
         };
-    };
-
-    data.push(trace);
-};
+    }};
 
 
 
 
-function init() {
-// Information for first trace
-// Also information for first bubble chart and metadata
-Plotly.newplot('bar', bar_data[0]);
-Plotly.newplot('bubble', bubble_data[0]);
-};
+init();
 
+d3.selectAll("#selDataset").on("onchange", optionChanged);    
 
-
-// To create updatePlotly function, we start with variables dropdownMenu and dataset.
-// Use first trace to initialize page. Give options for datasets as conditional. Can begin here
-// once necessary values are identified above.
-
-
-
-d3.selectAll("#selDataset").on("onchange", updatePlotly)
-
-function updatePlotly() {
+function optionChanged() {
     let dropdownMenu = d3.selectAll("#selDataset");
-    let dataset = dropdownMenu.property("value");
+    let dataset = dropdownMenu.property(Names);
+        
+    //let x = [];
+    //let y = [];
     
-    let x = [];
-    let y = [];
-
-    // New dataset options
-
-
-
-
-    // Restyle
     Plotly.restyle("plot","x",[x])
-    Plotly.restyle("plot","y",[y])
-};
-
-  init();  
+    Plotly.restyle("plot","y",[y])};
